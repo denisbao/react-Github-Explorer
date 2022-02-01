c19b79998e7c51e443a880d9649f2cd7932cf43e
denisbao <denis.tpd@gmail.com>
2022-02-01 16:08:09 -0300

    Configurando o Typescript no projeto

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,9 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 import { useState, useEffect } from "react";
 
 
+interface Repository {
+  name: string,
+  description: string,
+  html_url: string
+}
+
 export function RepositoryList() {
 
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(()=>{
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -15,7 +21,7 @@ export function RepositoryList() {
 
   return (
     <section className="repository-list">
-      <h1>Lista de Repositórios de: {repositories[0].owner?.login}</h1>
+      <h1>Lista de Repositórios</h1>
       <ul>
         {repositories.map(repository => {
           return <RepositoryItem key={repository.name} repository={repository}/>
